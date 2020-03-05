d258c12c554c1026638a24e9e1fcc443178b6108
Daniel Sturm <d.sturm@zirkeldesign.de>
2020-03-05 12:46:27 +0100

    Add font

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -2,7 +2,10 @@
  * External Dependencies
  */
 import 'jquery';
-import 'bootstrap';
+// import 'bootstrap';
+
+import "typeface-arbutus-slab";
+import "typeface-open-sans";
 
 $(document).ready(() => {
   // console.log('Hello world');
