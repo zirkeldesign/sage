c8bad5df02f9238fce98a5788b0a0f80423a1d03
Daniel Sturm <d.sturm@zirkeldesign.de>
2020-03-10 16:11:59 +0100

    Add resolver for page templates

--- app/setup.php
+++ app/setup.php
@@ -150,3 +150,17 @@ add_action('widgets_init', function () {
         'id' => 'sidebar-footer'
     ] + $config);
 });
+
+add_action( 'graphql_register_types', function() {
+    register_graphql_field(
+        'Page',
+        'pageTemplate',
+        [
+            'type' => 'String',
+            'description' => 'WordPress Page Template',
+            'resolve' => function ($page) {
+                return get_page_template_slug($page->pageId);
+            },
+        ]
+    );
+} );
